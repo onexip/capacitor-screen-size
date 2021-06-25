de1f757d75067e0cf5f6defe43551e813948ed4b
onexip_vanessa <bruell@onexip.com>
2021-06-25 10:11:16 +0200

    adjusted native code

--- android/src/main/java/com/onexip/plugins/capacitorscreensize/ScreenSizePlugin.java
+++ android/src/main/java/com/onexip/plugins/capacitorscreensize/ScreenSizePlugin.java
@@ -5,6 +5,7 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import android.util.DisplayMetrics;
 
 @CapacitorPlugin(name = "ScreenSize")
 public class ScreenSizePlugin extends Plugin {
--- ios/Plugin/ScreenSizePlugin.swift
+++ ios/Plugin/ScreenSizePlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import DevicePpi
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
