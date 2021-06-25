fa4476545818d7582ea630005593ad00f0774c8b
onexip_vanessa <bruell@onexip.com>
2021-06-25 10:11:16 +0200

    adjusted native code

--- ios/Plugin/ScreenSize.swift
+++ ios/Plugin/ScreenSize.swift
@@ -1,7 +1,26 @@
 import Foundation
+import DevicePpi
 
 @objc public class ScreenSize: NSObject {
     @objc public func echo(_ value: String) -> String {
         return value
+    }
+
+    @objc public func getDevicePPI() -> Double  {
+        let ppi: Double = {
+            switch Ppi.get() {
+            case .success(let ppi):
+                return ppi
+            case .unknown(let bestGuessPpi, let error):
+                // A bestGuessPpi value is provided but may be incorrect
+                // Treat as a non-fatal error -- e.g. log to your backend and/or display a message
+                print(error)
+                return bestGuessPpi
+            }
+        }()
+        print("#### Plugin - Device PPI: ", ppi)
+
+        return ppi
+
     }
 }
